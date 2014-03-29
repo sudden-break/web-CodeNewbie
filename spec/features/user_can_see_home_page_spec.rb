3fa2e42bb019a0e4c84861395001aabe70e13d40
sarony <saron.yitbarek@gmail.com>
2014-03-29 13:57:13 -0400

    pass user_can_see_homepage can_read_how_does_this_work_section spec

--- spec/features/user_can_see_home_page_spec.rb
+++ spec/features/user_can_see_home_page_spec.rb
@@ -7,4 +7,9 @@ feature 'User can see homepage' do
     expect(page).to have_content('About')
   end  
 
+  scenario 'and view "How Does This Work" section' do
+    visit root_path
+    expect(page).to have_content('How Does This Work')
+  end
+
 end
