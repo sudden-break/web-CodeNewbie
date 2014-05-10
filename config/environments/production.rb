0008c4cac619609b0a85e914208520192dda2748
sarony <saron.yitbarek@gmail.com>
2014-05-10 19:52:32 -0400

    precompiling assets for heroku added loggin setting to config.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,8 @@ CodeNewbie::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].to_sym : ('info').to_sym
+
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -84,7 +85,6 @@ CodeNewbie::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.log_level = ENV['LOG_LEVEL'] ? ENV['LOG_LEVEL'].to_sym : ('info').to_sym
 
   config.action_mailer.default_url_options = { host: 'codenewbie.com' }
 end
