427d7b7d8d5d6ce3b354923a2ca160c001af96f4
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-06-08 20:13:11 -0400

    Fixes capybara / rspec setup and example settings, updates seeds.rb file.

--- spec/features/user_can_see_challenge_spec.rb
+++ spec/features/user_can_see_challenge_spec.rb
@@ -1,21 +1,25 @@
 require 'spec_helper'
 
-feature 'User can see monthly challenge' do 
-  let(:challenge){ FactoryGirl.create(:valid_challenge, :slug => "the-commit") }
-
-  before(:all) do 
-    # Challenge.any_instance.should_return(:challenge)
+feature 'User can see monthly challenge' do
+  before(:all) do
+    @challenge = FactoryGirl.create(
+      :valid_challenge, slug: "the-commit", hashtag: '#TheCommit')
   end
+
+  # before(:all) do
+  #   # Challenge.any_instance.should_return(:challenge)
+  # end
 
   scenario 'from homepage' do
     visit root_path
+    # => Page render is blowing up saying @challenge = nil
 
-    expect(page).to have_content('theCommit')
+    expect(page).to have_content('#TheCommit')
     expect(page).to have_link('Learn More', href: "/challenges/the-commit")
 
     click_link("Learn More")
 
     expect(page).to have_content("June Challenge")
-    expect(page).to have_content("#theCommit")
-  end  
+    expect(page).to have_content("#TheCommit")
+  end
 end
