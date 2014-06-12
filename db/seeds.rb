ba94b85f605a9ae9e6a500c1595962cb4ddb7011
sarony <saron.yitbarek@gmail.com>
2014-06-11 20:26:51 -0400

    Changed copy for #theCommit in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -191,7 +191,7 @@ jobs = Job.create([
 challenges = Challenge.create([
   {
     name: "The Commit",
-    source: "We got this idea from John Resig's awesome blog post 'Write Code Every Day'. Read it <a href='http://ejohn.org/blog/write-code-every-day/'>here</a>",
+    source: "We got the hastag from the awesome <a href='https://twitter.com/danielchangNYC'>Dan Chang</a> and this idea from John Resig's awesome blog post 'Write Code Every Day'. Read it <a href='http://ejohn.org/blog/write-code-every-day/'>here</a>",
     description: "Make one commit every day. It can be small, big, and in any language you want. And when you do, tell us about it. Tweet us at <a href='https://twitter.com/codenewbies'>@CodeNewbies</a> with the hashtag <a href='http://https://twitter.com/search?f=realtime&q=%23thecommit&src=typd'>#theCommit</a>. Happy coding!",
     slug: "the-commit",
     current: true,
