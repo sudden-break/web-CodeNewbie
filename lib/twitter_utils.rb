ed8d54598018473c97c856fb8f79ccbc2467dd73
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-06-17 02:44:58 -0400

    Fixes DM Time.now output on server by using localized time zone.

--- lib/twitter_utils.rb
+++ lib/twitter_utils.rb
@@ -60,7 +60,7 @@ module TwitterUtils
       tweet_url = "https://twitter.com/#{t.attrs[:user][:screen_name]}/status/#{t.attrs[:id]}"
 
       dm_text   = "--- A tracked HashTag was used!\n"
-      dm_text  += "#{t.created_at.strftime('Created on %m/%d/%Y at %I:%M%p.')}\n"
+      dm_text  += "#{t.created_at.in_time_zone(TIME_ZONE).strftime('Created on %m/%d/%Y at %I:%M%p.')}\n"
       dm_text  += "Tweet URL => #{tweet_url}"
       
       DM_RECIPIENTS.split(',').each do |recipient|
