b0539f3852fc008fa0c4e5f5fe1af420f213cb95
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-06-17 02:09:55 -0400

    Fixes Figaro issues with schedule.rb (need to find way to load EnvVars here).

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,28 @@
+# Allows for Server Time to map to Local Time.
+Time.zone = 'Eastern Time (US & Canada)'
+
+# Custom Variables to determine task frequency.
+follow_all_friends_times = 
+  ['12:15am','6:15am','12:15pm','6:15pm'].collect do |time|
+    Time.zone.parse(time).localtime
+  end
+
+# Information on Whenever Gem:
+# ==> http://github.com/javan/whenever
+
+set :output, 'logs/cron.log'
+
+every 30.minutes do
+  rake 'cron:send_message_on_hashtag'
+end
+
+every :day, :at => follow_all_friends_times do
+  rake 'cron:follow_all_friends'
+end
+
+# Find a way to pull in Figaro EnvVars w/o loading application.yaml
+
+<<-WHENEVERTODO
 # Load Environment Settings.
 require 'active_support'
 require 'yaml'
@@ -6,25 +31,7 @@ settings = YAML::load_file(
   File.join(File.dirname(File.expand_path(__FILE__)), '/application.yml')
 )
 
-# Allows for Server Time to map to Local Time.
 Time.zone = settings['LOCAL_TIME_ZONE']
-
-# Custom Variables to determine Frequency
-follow_all_friends_times = 
-  ['12:15am','6:15am','12:15pm','6:15pm'].collect do |time|
-    Time.zone.parse(time).localtime
-  end
 time_diff = eval(settings['TIME_DIFF']) rescue 1.hour
 
-# Information on Whenever Gem:
-# ==> http://github.com/javan/whenever
-
-set :output, 'logs/cron.log'
-
-every time_diff do
-  rake 'cron:send_message_on_hashtag'
-end
-
-every :day, :at => follow_all_friends_times do
-  rake 'cron:follow_all_friends'
-end
+WHENEVERTODO
