585b69c56b5c3ba84ca198d32203b23931dbc37c
sarony <saron.yitbarek@gmail.com>
2014-06-17 23:33:47 -0400

    Changes code for next twitter chat date

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authorize
 
   def index
-    @next_wednesday = Chronic.parse("next Wednesday").strftime("%B %d")
+    @next_wednesday = Chronic.parse("this Wednesday").strftime("%B %d")
     @challenge = Challenge.find_by(:current => true)
   end
 
