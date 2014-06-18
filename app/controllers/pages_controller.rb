d687aa5aa5333191ddcf004440bd2c9231da8241
sarony <saron.yitbarek@gmail.com>
2014-06-18 07:28:17 -0400

    Fixes next chat day to allow for today being Wed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authorize
 
   def index
-    @next_wednesday = Chronic.parse("this Wednesday").strftime("%B %d")
+    @next_wednesday = Time.now.wday == 3 ? Chronic.parse("today").strftime("%B %d") : Chronic.parse("this Wednesday").strftime("%B %d")
     @challenge = Challenge.find_by(:current => true)
   end
 
