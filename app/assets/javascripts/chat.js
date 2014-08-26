fc19f68e027467464c85bf732bbdb90b60e9b7c2
sarony <saron.yitbarek@gmail.com>
2014-08-26 15:32:13 -0400

    Adds tweet and email logic to blog show page

---
+++ app/assets/javascripts/chat.js
@@ -0,0 +1,6 @@
+$(function(){ 
+  $('.popup').click(function(event){
+    event.preventDefault();
+    window.open($(this).attr("href"), "popupWindow", "width=600,height=600,scrollbars=yes");
+  });
+});
