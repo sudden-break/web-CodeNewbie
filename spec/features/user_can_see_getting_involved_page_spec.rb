3d560249604900f99507d07bc26b875d02a02087
sarony <saron.yitbarek@gmail.com>
2014-09-26 00:50:34 -0400

    Adds links and conditionals for header partial

--- spec/features/user_can_see_getting_involved_page_spec.rb
+++ spec/features/user_can_see_getting_involved_page_spec.rb
@@ -5,7 +5,7 @@ feature "User can see getting_involved page" do
   scenario "by going to the link" do 
     visit get_involved_path
 
-    expect(page).to have_content("How To Get Involved")
+    expect(page).to have_content("Get Involved")
   end
  
 end
