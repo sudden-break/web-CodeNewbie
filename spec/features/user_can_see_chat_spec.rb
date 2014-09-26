db81b0f3a27ef6b2c8916b8de20ff217a7ad23bd
sarony <saron.yitbarek@gmail.com>
2014-09-26 00:01:35 -0400

    Passes all specs

--- spec/features/user_can_see_chat_spec.rb
+++ spec/features/user_can_see_chat_spec.rb
@@ -5,8 +5,7 @@ feature "Use can see chat" do
   scenario "by going to the chat page" do 
 
     visit chat_info_path
-
-    expect(page).to have_content("Our Weekly TwitterChat")
+    expect(page).to have_content("TwitterChat")
 
   end
 
