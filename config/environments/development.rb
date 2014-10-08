6cd039ad77aa7d1ec6c43e89fbe5f3ed285e1780
sarony <saron.yitbarek@gmail.com>
2014-10-07 22:04:41 -0400

    Changes route for editing password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ CodeNewbie::Application.configure do
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
-    :enable_starttls_auto => true, # detects and uses STARTTLS
+    # :enable_starttls_auto => true, # detects and uses STARTTLS
     :user_name => ENV["MANDRILL_USERNAME"],
     :password  => ENV["MANDRILL_API_KEY"], # SMTP password is any valid API key
     :authentication => 'login', # Mandrill supports 'plain' or 'login'
