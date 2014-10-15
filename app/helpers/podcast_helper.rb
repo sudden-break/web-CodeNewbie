bb63d150cc073bec6822cdfa237bcb66795f0e23
sarony <saron.yitbarek@gmail.com>
2014-10-15 17:41:05 -0400

    Allows multiple guest names and adds brian's photo

--- app/helpers/podcast_helper.rb
+++ app/helpers/podcast_helper.rb
@@ -1,9 +1,11 @@
 module PodcastHelper
 
   def guest_name(episode)
-    episode.guests.select{|guest| guest.first_name != "Saron"}.each do |guest|
-      return guest.full_name
+    guests = episode.guests.select{|guest| guest.first_name != "Saron"}
+    names = guests.collect do |guest|
+      guest.full_name
     end
+    names.join(", ")
   end
   
 end
